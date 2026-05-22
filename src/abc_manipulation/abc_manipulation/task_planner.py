42b0fa0b14c4496f465e532f625a26c8da49aeac
NTK0203 <newthink0203@naver.com>
2026-05-22 13:59:12 +0900

    feat: YOLO 비전 탐지 action 인터페이스 연동

--- src/abc_manipulation/abc_manipulation/task_planner.py
+++ src/abc_manipulation/abc_manipulation/task_planner.py
@@ -4,7 +4,7 @@ from rclpy.action import ActionClient
 from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.executors import MultiThreadedExecutor
 
-from abc_interfaces.msg import PurchaseItem
+from abc_interfaces.msg import PurchaseItem, DetectionArray
 from abc_interfaces.action import PickItem, ScanBarcode, PlaceItem
 
 class TaskPlannerNode(Node):
@@ -12,24 +12,49 @@ class TaskPlannerNode(Node):
         super().__init__('task_planner_node')
         self.cb_group = ReentrantCallbackGroup()
 
+        # YOLO 처리 후 받아올 subscriber 설정
+        self.sub_detections = self.create_subscription(
+            DetectionArray,
+            '/detections',
+            self.detection_callback,
+            10, #메시지 큐 size는 임의로 10으로 설정해놓음.
+            callback_group=self.cb_group
+        )
+
         # 클라이언트 설정
         self.pick_client = ActionClient(self, PickItem, 'pick_item', callback_group=self.cb_group)
         self.scan_client = ActionClient(self, ScanBarcode, 'scan_barcode', callback_group=self.cb_group)
         self.place_client = ActionClient(self, PlaceItem, 'place_item', callback_group=self.cb_group)
 
         self.get_logger().info("Task Planner Node Started.")
-        self.create_timer(1.0, self.run_sequence, callback_group=self.cb_group)
         self.is_running = False
+        self.detections_arr = []
+        self.create_timer(1.0, self.run_sequence, callback_group=self.cb_group)
+
+    #YOLO에서 받아온 탐지 결과 대기열 처리
+    def detection_callback(self, msg):
+        if not self.is_running and not self.detections_arr:
+            if len(msg.detections) > 0:
+                self.detection_arr = msg.detections
+                self.get_logger().info(f"새로운 주문 상품 수신: {len(msg.detections)}개 물품을 받아왔습니다.")
 
     async def run_sequence(self):
-        if self.is_running: return
+        if self.is_running or not self.detection_arr:
+            return
         self.is_running = True
 
-        # 테스트 데이터: 2345 (포카리스웨트) 한 개 주문
-        items = [PurchaseItem(product_id=2345, quantity=1)]
+        items = self.detection_arr
+        self.detection_arr = []
 
         for item in items:
-            self.get_logger().info(f"\n==== [처리 시작] ID: {item.product_id} ====")
+            
+            barcode = str(item.class_name).strip()
+            cx = float(item.center_x)
+            cy = float(item.center_y)
+            w = float(item.width)
+            h = float(item.height)
+
+            self.get_logger().info(f"\n==== [처리 시작] ID: {barcode} ==== ")
 
             # 1. PickItem 노드 통신
             self.get_logger().info(">> Pick 서버 연결 대기...")
@@ -38,7 +63,12 @@ class TaskPlannerNode(Node):
                 break
             
             self.get_logger().info(">> pick_item 노드 호출: 물체 인식 및 파지 명령")
-            goal_msg = PickItem.Goal(product_id=item.product_id)
+            goal_msg = PickItem.Goal(
+                center_x=cx,
+                center_y=cy,
+                width=w,
+                height=h
+            )
             
             # 결과 응답 대기
             res1 = await self.call_action(self.pick_client, goal_msg)
