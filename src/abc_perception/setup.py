60fe5c953c2ae9ac9c607abf24fdc2e6f37fd5bd
kdm1258 <kdm1258@naver.com>
2026-05-22 15:38:00 +0900

    feat: add perception launch

--- src/abc_perception/setup.py
+++ src/abc_perception/setup.py
@@ -1,5 +1,7 @@
 from setuptools import find_packages, setup
+from glob import glob
 import os
+
 package_name = 'abc_perception'
 
 setup(
@@ -11,6 +13,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         # [핵심] 모델 파일을 패키지의 share 디렉토리로 설치
         (os.path.join('share', package_name, 'models'), ['models/best.pt']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
